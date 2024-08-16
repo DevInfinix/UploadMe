03cb0feeb49501b49acbbf8d9f66cfe240d6b8b4
1xtAsh <ashraykrsingh321@gmail.com>
2024-08-16 20:18:12 +0530

    upload: Refactor

--- upload.sh
+++ upload.sh
@@ -6,10 +6,11 @@
 echo " "
 echo "[1] Github Release [gh auth login]
 [2] Devuploads [Key]
-[3] Temp.sh
-[4] pixeldrain
+[3] pixeldrain [Key]
+[4] Temp.sh
 [5] Gofile
 [6] oshi.at
+[7] Sourceforge [Key]
 "
 read -p "Please enter your number: " UP
 read -p "Please enter file path/name: " FP
@@ -29,20 +30,14 @@ bash <(curl -s https://devuploads.com/upload.sh) -f $FP -k $KEY
 fi
 
 if [ $UP == 3 ]; then
-echo -e "Started uploading file on Temp..."
-curl -T $FP temp.sh
+read -p "Please enter Pixel Drain key: " KEY
+echo -e "Started uploading file on PixelDrain..."
+curl -T $FP -u ":$KEY" https://pixeldrain.com/api/file/
 fi
 
 if [ $UP == 4 ]; then
-#read -p "Please enter Pixel Drain key: " KEY
-echo -e "Started uploading file on PixelDrain..."
-ls go-pd || PixelDrain=1
-if [ "$PixelDrain" == "1" ]; then
-wget https://github.com/ManuelReschke/go-pd/releases/download/v1.4.0/go-pd_1.4.0_linux_amd64.tar.gz
-tar -xf go-pd*
-rm -rf go-pd_*.tar.gz
-fi
-./go-pd upload $FP
+echo -e "Started uploading file on Temp..."
+curl -T $FP temp.sh
 fi
 
 if [ $UP == 5 ]; then
@@ -54,4 +49,12 @@ fi
 if [ $UP == 6 ]; then
 echo -e "Started uploading file on Oshi.at..."
 curl -T $FP https://oshi.at
+fi
+
+if [ $UP == 7 ]; then
+echo -e "Started uploading file on Sourceforge..."
+read -p "Please enter Username: " USER
+read -p "Please enter upload location:
+Note: Path after /home/frs/project/" UPL
+scp $FP "$USER"@frs.sourceforge.net:/home/frs/project/$UPL
 fi
